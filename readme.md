0f33f909e05dec497380907ca27c55ef39db4e13
Jonas Obrist <jonas.obrist@hde.co.jp>
2017-04-14 12:39:23 +0900

    added editorconfig, readme

---
+++ readme.md
@@ -0,0 +1,5 @@
+# AsyncIO DynamoDB
+
+*Experimental* asynchronous dynamodb object mapper built on top of `attrs`.
+
+Use at own risk, I will change the API without warning.
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(
-    version='1.0',
+    version='0.1',
     name='aiodynamo',
     package_dir={'': 'src'},
     packages=find_packages(where='src'),
