2c109a2ee377f92856843868f5ce2f1c8a87e6b0
Jonas Obrist <jonas.obrist@hde.co.jp>
2017-04-17 19:07:06 +0900

    better tests

--- tests/tests.py
+++ tests/tests.py
@@ -12,31 +12,47 @@ from aiodynamo.exceptions import NotModified, NotFound
 from aiodynamo.helpers import remove_empty_strings
 
 
-async def cleanup(client):
-    response = await client.list_tables()
-    for table in response['TableNames']:
-        await client.delete_table(TableName=table)
+@attr.s
+class ConnectionContext:
+    models = attr.ib()
+
+    @staticmethod
+    def table_name(model):
+        return f'table-{model.__name__}'
+
+    async def __aenter__(self):
+        try:
+            endpoint_url = os.environ['DYNAMODB_ENDPOINT_URL']
+        except KeyError:
+            raise pytest.skip('No endpoint url specified')
+        client = get_session().create_client(
+            'dynamodb',
+            endpoint_url=endpoint_url,
+            region_name='fake-region',
+            aws_access_key_id='fake-aws-access-key-id',
+            aws_secret_access_key='fake-aws-secret-access-key',
+        )
+        router = {
+            model: self.table_name(model)
+            for model in self.models
+        }
+        self.db = Connection(router=router, client=client)
+        await asyncio.wait([
+            self.db.create_table(model, read_cap=5, write_cap=5)
+            for model in self.models
+        ])
+        return self.db
+
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        await asyncio.wait([
+            self.db.client.delete_table(TableName=self.table_name(model))
+            for model in self.models
+        ])
+        self.db.client.close()
 
 
-@pytest.fixture()
-def dynamo_client():
-    try:
-        endpoint_url = os.environ['DYNAMODB_ENDPOINT_URL']
-    except KeyError:
-        raise pytest.skip('No endpoint url specified')
-    client = get_session().create_client(
-        'dynamodb',
-        endpoint_url=endpoint_url,
-        region_name='us-east-1',
-        aws_access_key_id='local',
-        aws_secret_access_key='local',
-    )
-    try:
-        yield client
-    finally:
-        loop = asyncio.get_event_loop()
-        loop.run_until_complete(cleanup(client))
-        client.close()
+def connection(*models):
+    return ConnectionContext(models)
 
 
 def runner(func):
@@ -49,29 +65,24 @@ def runner(func):
 
 
 @runner
-async def test_basic(dynamo_client):
+async def test_basic():
     @model(keys=Keys.HashRange)
     class MyModel:
         r = range_key(str)
         h = hash_key(str)
 
-    router = {
-        MyModel: 'my-table'
-    }
-
-    db = Connection(router=router, client=dynamo_client)
-    await db.create_table(MyModel, read_cap=5, write_cap=5)
-    instance = MyModel(r='r', h='h')
-    await db.save(instance)
-    db_instance = await db.lookup(MyModel, h='h', r='r')
-    assert instance == db_instance
-    await db.delete(db_instance)
-    with pytest.raises(NotFound):
-        await db.lookup(MyModel, h='h', r='r')
+    async with connection(MyModel) as db:
+        instance = MyModel(r='r', h='h')
+        await db.save(instance)
+        db_instance = await db.lookup(MyModel, h='h', r='r')
+        assert instance == db_instance
+        await db.delete(db_instance)
+        with pytest.raises(NotFound):
+            await db.lookup(MyModel, h='h', r='r')
 
 
 @runner
-async def test_update(dynamo_client):
+async def test_update():
     @model(keys=Keys.HashRange)
     class MyModel:
         r = range_key(str)
@@ -79,131 +90,112 @@ async def test_update(dynamo_client):
         a = field()
         b = field()
 
-    router = {
-        MyModel: 'my-table'
-    }
-
-    db = Connection(router=router, client=dynamo_client)
-    await db.create_table(MyModel, read_cap=5, write_cap=5)
-    instance = MyModel(r='r', h='h', a='a', b='b')
-    await db.save(instance)
-    modified = instance.modify(a= 'not a')
-    with pytest.raises(NotModified):
-        await db.update(instance)
-    await db.update(modified)
-    db_instance = await db.lookup(MyModel, r='r', h='h')
-    assert db_instance == MyModel(r='r', h='h', a='not a', b='b')
+    async with connection(MyModel) as db:
+        instance = MyModel(r='r', h='h', a='a', b='b')
+        await db.save(instance)
+        modified = instance.modify(a= 'not a')
+        with pytest.raises(NotModified):
+            await db.update(instance)
+        await db.update(modified)
+        db_instance = await db.lookup(MyModel, r='r', h='h')
+        assert db_instance == MyModel(r='r', h='h', a='not a', b='b')
 
 
 @runner
-async def test_query(dynamo_client):
+async def test_query():
     @model(keys=Keys.HashRange)
     class MyModel:
         r = range_key(str)
         h = hash_key(str)
         x = field()
-    router = {
-        MyModel: 'my-table'
-    }
-    db = Connection(router=router, client=dynamo_client)
-    await db.create_table(MyModel, read_cap=5, write_cap=5)
-    instances = [
-        MyModel(r=f'r{index}', h='h', x=index) for index in range(10)
-    ]
-    await asyncio.wait(map(db.save, instances))
-    db_instances = [instance async for instance in db.query(MyModel, h='h')]
-    assert instances == db_instances
+
+    async with connection(MyModel) as db:
+        instances = [
+            MyModel(r=f'r{index}', h='h', x=index) for index in range(10)
+        ]
+        await asyncio.wait(map(db.save, instances))
+        db_instances = [instance async for instance in db.query(MyModel, h='h')]
+        assert instances == db_instances
 
 
 @runner
-async def test_fixed_hash(dynamo_client):
+async def test_fixed_hash():
     @model(keys=Keys.HashRange)
     class MyModel:
         h = hash_key(str, constant='hashkey')
         r = range_key(str)
-    router = {
-        MyModel: 'my-table'
-    }
-    db = Connection(router=router, client=dynamo_client)
-    await db.create_table(MyModel, read_cap=5, write_cap=5)
-    instance = MyModel(r='test')
-    assert instance.h == 'hashkey'
-    await db.save(instance)
-    db_instance = await db.lookup(MyModel, r='test')
-    assert instance == db_instance
-    assert db_instance.h == 'hashkey'
+
+    async with connection(MyModel) as db:
+        instance = MyModel(r='test')
+        assert instance.h == 'hashkey'
+        await db.save(instance)
+        db_instance = await db.lookup(MyModel, r='test')
+        assert instance == db_instance
+        assert db_instance.h == 'hashkey'
 
 
 @runner
-async def test_alias(dynamo_client):
+async def test_alias():
     @model(keys=Keys.Hash)
     class MyModel:
         h = hash_key(str, alias='hash_key')
-    router = {
-        MyModel: 'my-table'
-    }
-    db = Connection(router=router, client=dynamo_client)
-    await db.create_table(MyModel, read_cap=5, write_cap=5)
-    response = await dynamo_client.describe_table(TableName='my-table')
-    assert response['Table']['KeySchema'] == [{
-        'AttributeName': 'hash_key',
-        'KeyType': 'HASH'
-    }]
-    instance = MyModel(h='h')
-    await db.save(instance)
-    db_instance = await db.lookup(MyModel, h='h')
-    assert instance == db_instance
+
+    async with connection(MyModel) as db:
+        response = await db.client.describe_table(
+            TableName=ConnectionContext.table_name(MyModel)
+        )
+        assert response['Table']['KeySchema'] == [{
+            'AttributeName': 'hash_key',
+            'KeyType': 'HASH'
+        }]
+        instance = MyModel(h='h')
+        await db.save(instance)
+        db_instance = await db.lookup(MyModel, h='h')
+        assert instance == db_instance
 
 
 @runner
-async def test_alias_hr(dynamo_client):
+async def test_alias_hr():
     @model(keys=Keys.HashRange)
     class MyModel:
         h = hash_key(str, alias='hash_key')
         r = range_key(str, alias='range_key')
-    router = {
-        MyModel: 'my-table'
-    }
-    db = Connection(router=router, client=dynamo_client)
-    await db.create_table(MyModel, read_cap=5, write_cap=5)
-    response = await dynamo_client.describe_table(TableName='my-table')
-    assert response['Table']['KeySchema'] == [{
-        'AttributeName': 'hash_key',
-        'KeyType': 'HASH'
-    },{
-        'AttributeName': 'range_key',
-        'KeyType': 'RANGE'
-    }]
-    instance = MyModel(h='hv', r='rv')
-    await db.save(instance)
-    db_instance = await db.lookup(MyModel, h='hv', r='rv')
-    assert instance == db_instance
 
+    async with connection(MyModel) as db:
+        response = await db.client.describe_table(
+            TableName=ConnectionContext.table_name(MyModel)
+        )
+        assert response['Table']['KeySchema'] == [{
+            'AttributeName': 'hash_key',
+            'KeyType': 'HASH'
+        },{
+            'AttributeName': 'range_key',
+            'KeyType': 'RANGE'
+        }]
+        instance = MyModel(h='hv', r='rv')
+        await db.save(instance)
+        db_instance = await db.lookup(MyModel, h='hv', r='rv')
+        assert instance == db_instance
 
 
 @runner
-async def test_non_field(dynamo_client):
+async def test_non_field():
     @model(keys=Keys.Hash)
     class MyModel:
         key = hash_key(str)
         attr = attr.ib(default='not-attr')
 
-    router = {
-        MyModel: 'my-table'
-    }
-    db = Connection(router=router, client=dynamo_client)
-    await db.create_table(MyModel, read_cap=5, write_cap=5)
-
-    instance = MyModel(key='mykey', attr='attr')
-    await db.save(instance)
-    db_instance = await db.lookup(MyModel, key='mykey')
-    assert db_instance != instance
-    assert db_instance.attr == 'not-attr'
-    assert attr.assoc(db_instance, attr='attr') == instance
+    async with connection(MyModel) as db:
+        instance = MyModel(key='mykey', attr='attr')
+        await db.save(instance)
+        db_instance = await db.lookup(MyModel, key='mykey')
+        assert db_instance != instance
+        assert db_instance.attr == 'not-attr'
+        assert attr.assoc(db_instance, attr='attr') == instance
 
 
 @runner
-async def test_to_from_db_convert(dynamo_client):
+async def test_to_from_db_convert():
     def prefixed(prefix: str):
         def to_db(suffix: str) -> str:
             return prefix + suffix
@@ -225,26 +217,22 @@ async def test_to_from_db_convert(dynamo_client):
         def unprefixed(self):
             return self.prefixed_key[4:]
 
-    router = {
-        MyModel: 'my-model'
-    }
-    db = Connection(router=router, client=dynamo_client)
-    await db.create_table(MyModel, read_cap=5, write_cap=5)
+    async with connection(MyModel) as db:
+        instance = MyModel(prefixed_key='suffix')
+        assert instance.prefixed_key == 'TESTsuffix'
+        assert instance.unprefixed == 'suffix'
+        await db.save(instance)
+        response = await db.client.get_item(
+            TableName=ConnectionContext.table_name(MyModel),
+            Key={'prefixed_key': {'S': 'TESTsuffix'}}
+        )
+        assert response['Item']['prefixed_key'] == {'S': 'TESTsuffix'}
+        db_instance = await db.lookup(MyModel, prefixed_key='suffix')
+        assert db_instance == instance
 
-    instance = MyModel(prefixed_key='suffix')
-    assert instance.prefixed_key == 'TESTsuffix'
-    assert instance.unprefixed == 'suffix'
-    await db.save(instance)
-    response = await db.client.get_item(
-        TableName='my-model',
-        Key={'prefixed_key': {'S': 'TESTsuffix'}}
-    )
-    assert response['Item']['prefixed_key'] == {'S': 'TESTsuffix'}
-    db_instance = await db.lookup(MyModel, prefixed_key='suffix')
-    assert db_instance == instance
 
 @runner
-async def test_auto_field(dynamo_client):
+async def test_auto_field():
     counter = itertools.count(start=1)
 
     @model(keys=Keys.Hash)
@@ -252,23 +240,17 @@ async def test_auto_field(dynamo_client):
         key = hash_key(str)
         auto_field = field(auto=lambda: next(counter), default=0)
 
-    router = {
-        MyModel: 'my-model'
-    }
-
-    db = Connection(router=router, client=dynamo_client)
-    await db.create_table(MyModel, read_cap=5, write_cap=5)
-
-    instance = MyModel(key='test')
-    assert instance.auto_field == 0
-    await db.save(instance)
-    db_instance = await db.lookup(MyModel, key='test')
-    assert db_instance != instance
-    assert db_instance.auto_field == 1
+    async with connection(MyModel) as db:
+        instance = MyModel(key='test')
+        assert instance.auto_field == 0
+        await db.save(instance)
+        db_instance = await db.lookup(MyModel, key='test')
+        assert db_instance != instance
+        assert db_instance.auto_field == 1
 
 
 @runner
-async def test_routing(dynamo_client):
+async def test_routing():
     @model(keys=Keys.Hash)
     class A:
         key = hash_key(str)
@@ -277,31 +259,23 @@ async def test_routing(dynamo_client):
     class B:
         key = hash_key(str)
 
-    router = {
-        A: 'table-a',
-        B: 'table-b',
-    }
+    async with connection(A, B) as db:
+        a = A(key='test')
+        b = B(key='test')
 
-    db = Connection(router=router, client=dynamo_client)
-    await db.create_table(A, read_cap=5, write_cap=5)
-    await db.create_table(B, read_cap=5, write_cap=5)
+        assert a != b
+        assert attr.asdict(a) == attr.asdict(b)
 
-    a = A(key='test')
-    b = B(key='test')
+        await db.save(a)
+        await db.save(b)
 
-    assert a != b
-    assert attr.asdict(a) == attr.asdict(b)
+        db_a = await db.lookup(A, key='test')
+        db_b = await db.lookup(B, key='test')
 
-    await db.save(a)
-    await db.save(b)
+        assert db_a != db_b
 
-    db_a = await db.lookup(A, key='test')
-    db_b = await db.lookup(B, key='test')
-
-    assert db_a != db_b
-
-    assert db_a == a
-    assert db_b == b
+        assert db_a == a
+        assert db_b == b
 
 
 @runner
@@ -317,21 +291,17 @@ async def test_routing(dynamo_client):
     [None],
     None
 ], ids=repr)
-async def test_empty_strings(dynamo_client, test_value):
+async def test_empty_strings(test_value):
     @model(keys=Keys.Hash)
     class MyModel:
         key = hash_key(str)
         value = field(default=type(test_value))
 
-    router = {
-        MyModel: 'my-model',
-    }
-    db = Connection(router=router, client=dynamo_client)
-    await db.create_table(MyModel, read_cap=5, write_cap=5)
-    instance = MyModel(key='key', value=test_value)
-    await db.save(instance)
-    db_instance = await db.lookup(MyModel, key='key')
-    assert db_instance == instance
+    async with connection(MyModel) as db:
+        instance = MyModel(key='key', value=test_value)
+        await db.save(instance)
+        db_instance = await db.lookup(MyModel, key='key')
+        assert db_instance == instance
 
 
 @pytest.mark.parametrize('value, expected', [
