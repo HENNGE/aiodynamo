56acb9593c7c58869455d503acc17c1cef62293f
Jonas Obrist <jonas.obrist@hde.co.jp>
2018-03-15 15:32:11 +0900

    0.0.9

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(
-    version='0.0.8',
+    version='0.0.9',
     name='aiodynamo',
     package_dir={'': 'src'},
     packages=find_packages(where='src'),
