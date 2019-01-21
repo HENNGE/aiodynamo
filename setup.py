168404719afbf461007fcc743b5693122dd47e79
Dima Tisnek <dimaqq@gmail.com>
2019-01-21 16:55:57 +0900

    Document dependency version choice

--- setup.py
+++ setup.py
@@ -8,6 +8,9 @@ setup(
     packages=find_packages(where="src"),
     install_requires=[
         "attrs>=17.4.0",
+        # `aiobotocore` and `boto3` share `botocore` dependency.
+        # Their latest releases do not agree on `botocore` version.
+        # Thus, pin a workable combination of the two.
         "aiobotocore>=0.10.0",
         "boto3>=1.9.49",
         "aiohttp>=3.5.4",
