a776693244d2a5c6a5bb47ff793ad61e8d304fb0
Dima Tisnek <dimaqq@gmail.com>
2021-08-24 13:37:54 +0900

    fix aiodynamo-httpx

--- benchmarks/query/bench_aiodynamo_httpx.py
+++ benchmarks/query/bench_aiodynamo_httpx.py
@@ -1,21 +1,29 @@
 import asyncio
 
-from boto3.dynamodb.conditions import Key
 from httpx import AsyncClient
 from pyperf import Runner
 
-from aiodynamo.client import Client
+from aiodynamo.client import Client, URL
 from aiodynamo.credentials import Credentials
-from aiodynamo.http import HTTPX
-from utils import TABLE_NAME, KEY_FIELD, KEY_VALUE, REGION_NAME
+from aiodynamo.http.httpx import HTTPX
+from aiodynamo.expressions import HashKey
+from utils import TABLE_NAME, KEY_FIELD, KEY_VALUE, REGION_NAME, ENDPOINT_URL
 
 
 async def inner():
     async with AsyncClient() as http_client:
-        client = Client(HTTPX(http_client), Credentials.auto(), REGION_NAME)
+        client = Client(
+            HTTPX(http_client),
+            Credentials.auto(),
+            region=REGION_NAME,
+            endpoint=URL(ENDPOINT_URL) if ENDPOINT_URL else None,
+        )
         items = [
             item
-            async for item in client.query(TABLE_NAME, Key(KEY_FIELD).eq(KEY_VALUE))
+            async for item in client.query(
+                TABLE_NAME,
+                key_condition=HashKey(KEY_FIELD, KEY_VALUE),
+            )
         ]
 
 
