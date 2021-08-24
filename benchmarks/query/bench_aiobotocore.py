3ea69065fc95b34c913287e77738f47aa68bef72
Dima Tisnek <dimaqq@gmail.com>
2021-08-24 13:48:42 +0900

    fix aiobotocore benchmark

--- benchmarks/query/bench_aiobotocore.py
+++ benchmarks/query/bench_aiobotocore.py
@@ -1,32 +1,33 @@
 import asyncio
 
-from aiobotocore import get_session
+from aiobotocore.session import get_session
 from pyperf import Runner
 
-from utils import TABLE_NAME, KEY_FIELD, KEY_VALUE, REGION_NAME
+from utils import TABLE_NAME, KEY_FIELD, KEY_VALUE, REGION_NAME, ENDPOINT_URL
 
 
 async def inner():
-    client = get_session().create_client("dynamodb", region_name=REGION_NAME)
     items = []
     lek = None
-    while True:
-        if lek is None:
-            kwargs = {}
-        else:
-            kwargs = {"ExclusiveStartKey": lek}
-        response = await client.query(
-            TableName=TABLE_NAME,
-            KeyConditionExpression="#k = :v",
-            ExpressionAttributeNames={"#k": KEY_FIELD},
-            ExpressionAttributeValues={":v": {"S": KEY_VALUE}},
-            **kwargs
-        )
-        items.extend(response["Items"])
-        lek = response.get("LastEvaluatedKey", None)
-        if lek is None:
-            break
-    await client.close()
+    async with get_session().create_client(
+        "dynamodb", region_name=REGION_NAME, endpoint_url=ENDPOINT_URL
+    ) as client:
+        while True:
+            if lek is None:
+                kwargs = {}
+            else:
+                kwargs = {"ExclusiveStartKey": lek}
+            response = await client.query(
+                TableName=TABLE_NAME,
+                KeyConditionExpression="#k = :v",
+                ExpressionAttributeNames={"#k": KEY_FIELD},
+                ExpressionAttributeValues={":v": {"S": KEY_VALUE}},
+                **kwargs
+            )
+            items.extend(response["Items"])
+            lek = response.get("LastEvaluatedKey", None)
+            if lek is None:
+                break
 
 
 def query_aiobotocore():
