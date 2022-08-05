a240f3ee3468235c0fec7c53bcf3bd330e582c93
Nicolas Zein <nicolas.zein@ze.delivery>
2022-08-05 16:03:41 -0300

    Add transact write items method to client

---
+++ src/aiodynamo/operations.py
@@ -0,0 +1,110 @@
+import abc
+from dataclasses import dataclass
+from typing import Any, Dict, Optional
+
+from aiodynamo.errors import EmptyItem
+from aiodynamo.expressions import Condition, Parameters, UpdateExpression
+from aiodynamo.types import Item, TableName
+from aiodynamo.utils import py2dy
+
+
+class Operation(metaclass=abc.ABCMeta):
+    @abc.abstractmethod
+    def to_request_payload(self) -> Dict[str, Any]:
+        pass
+
+
+@dataclass(frozen=True)
+class Put(Operation):
+    table: TableName
+    item: Item
+    condition: Optional[Condition] = None
+
+    def to_request_payload(self) -> Dict[str, Any]:
+        dynamo_item = py2dy(self.item)
+        if not dynamo_item:
+            raise EmptyItem()
+
+        payload: Dict[str, Any] = {
+            "TableName": self.table,
+            "Item": dynamo_item,
+        }
+        if self.condition:
+            params = Parameters()
+            payload["ConditionExpression"] = self.condition.encode(params)
+            payload.update(params.to_request_payload())
+
+        return {"Put": payload}
+
+
+@dataclass(frozen=True)
+class Update(Operation):
+    table: TableName
+    key: Item
+    expression: UpdateExpression
+    condition: Optional[Condition] = None
+
+    def to_request_payload(self) -> Dict[str, Any]:
+        params = Parameters()
+        expression = self.expression.encode(params)
+        if not expression:
+            raise EmptyItem()
+
+        payload: Dict[str, Any] = {
+            "TableName": self.table,
+            "UpdateExpression": expression,
+            "Key": py2dy(self.key),
+        }
+
+        if self.condition:
+            payload["ConditionExpression"] = self.condition.encode(params)
+
+        payload.update(params.to_request_payload())
+
+        return {"Update": payload}
+
+
+@dataclass(frozen=True)
+class Delete(Operation):
+    table: TableName
+    key: Item
+    condition: Optional[Condition] = None
+
+    def to_request_payload(self) -> Dict[str, Any]:
+        dynamo_item = py2dy(self.key)
+        if not dynamo_item:
+            raise EmptyItem()
+
+        payload: Dict[str, Any] = {
+            "TableName": self.table,
+            "Key": dynamo_item,
+        }
+        if self.condition:
+            params = Parameters()
+            payload["ConditionExpression"] = self.condition.encode(params)
+            payload.update(params.to_request_payload())
+
+        return {"Delete": payload}
+
+
+@dataclass(frozen=True)
+class ConditionCheck(Operation):
+    table: TableName
+    key: Item
+    condition: Optional[Condition] = None
+
+    def to_request_payload(self) -> Dict[str, Any]:
+        dynamo_item = py2dy(self.key)
+        if not dynamo_item:
+            raise EmptyItem()
+
+        payload: Dict[str, Any] = {
+            "TableName": self.table,
+            "Key": dynamo_item,
+        }
+        if self.condition:
+            params = Parameters()
+            payload["ConditionExpression"] = self.condition.encode(params)
+            payload.update(params.to_request_payload())
+
+        return {"ConditionCheck": payload}
