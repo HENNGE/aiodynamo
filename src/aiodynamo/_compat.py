8aa2122407838e119782ade1ce02676238851118
akawasaki <a.kawasaki1010@gmail.com>
2022-09-09 10:39:52 +0900

    removal: Protocal from _compat.

--- src/aiodynamo/_compat.py
+++ src/aiodynamo/_compat.py
@@ -2,8 +2,8 @@ import sys
 
 # cannot use try/except ImportError or mypy fails
 if sys.version_info >= (3, 8):
-    from typing import Literal, Protocol, TypedDict
+    from typing import Literal, TypedDict
 else:
-    from typing_extensions import Literal, Protocol, TypedDict
+    from typing_extensions import Literal, TypedDict
 
-__all__ = ("TypedDict", "Literal", "Protocol")
+__all__ = ("TypedDict", "Literal")
