f7180f47e6005c7b749bf371d05c096316da9620
Jonas Obrist <jonas.obrist@hennge.com>
2023-10-02 18:38:46 +0900

    fix mypy

--- src/aiodynamo/models.py
+++ src/aiodynamo/models.py
@@ -132,12 +132,19 @@ class GlobalSecondaryIndex(LocalSecondaryIndex):
     throughput: Optional[Throughput]
 
     def encode(self) -> EncodedGlobalSecondaryIndex:
-        # Cast due to https://github.com/python/mypy/issues/4122
+        # mypy really does not like an if/else assignment where the branches have
+        # different types, so we need to do some ridiculous casting here.
+        # The outer cst is due to https://github.com/python/mypy/issues/4122
+        # The two inner casts to Any are because of the if/else with different types
         return cast(
             EncodedGlobalSecondaryIndex,
             {
                 **super().encode(),
-                **(self.throughput.encode() if self.throughput else {}),
+                **(
+                    cast(Any, self.throughput.encode())
+                    if self.throughput
+                    else cast(Any, {})
+                ),
             },
         )
 
