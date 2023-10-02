7cfac34720e3052a1b8cf996727ff543db0f5fdf
Jonas Obrist <jonas.obrist@hennge.com>
2023-10-02 17:15:37 +0900

    Upgrade dependencies, drop 3.7 support, add 3.11 support

--- docs/conf.py
+++ docs/conf.py
@@ -61,7 +61,7 @@ master_doc = "index"
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = "en"
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -77,7 +77,7 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "alabaster"
+html_theme = "furo"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -173,3 +173,5 @@ epub_title = project
 
 # A list of files that should not be packed into the epub file.
 epub_exclude_files = ["search.html"]
+
+maximum_signature_line_length = 80
