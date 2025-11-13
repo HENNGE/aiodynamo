bbb5537a8ab3debdabf1cc614114fe26d637977f
Andrew Clemons <andrew.clemons@gmail.com>
2025-11-13 10:30:17 +0900

    Support dual-stack endpoints through configuration.
    AWS recently announced dual-stack endpoints for dynamodb:
    
    https://aws.amazon.com/about-aws/whats-new/2025/10/amazon-dynamodb-ipv6/
    
    Currently, we need to manually construct the url and pass a YARL object
    into aiodynamo's constructor as the `endpoint` arg:
    
    ```py
    endpoint = URL.build(
        scheme="https", host=f"dynamodb.{region}.api.aws", path="/"
    )
    
    client = Client(
        AIOHTTP(client_session),
        region=region,
        credentials=Credentials.auto(),
        endpoint=endpoint,
    )
    ```
    
    It would be convenient if aiodynamo supported a `dual_stack` flag like boto
    does.

--- src/aiodynamo/client.py
+++ src/aiodynamo/client.py
@@ -370,6 +370,7 @@ class Client:
     numeric_type: NumericTypeConverter = float
     throttle_config: RetryConfig = RetryConfig.default()
     health_monitor: HealthMonitor = NoOpHealthMonitor()
+    dual_stack: bool = False
 
     def table(self, name: str) -> Table:
         return Table(self, name)
@@ -984,6 +985,7 @@ class Client:
                     payload=payload,
                     action=action,
                     region=self.region,
+                    dual_stack=self.dual_stack,
                     endpoint=self.endpoint,
                 )
                 request_logger.debug("sending request %r", request)
